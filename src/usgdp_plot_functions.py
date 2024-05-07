b318f5d510615246e67cb442d67d89f43c74b77a
Jeffrey <jeffrey.guo@mail.utoronto.ca>
2024-05-07 12:44:41 -0400

    Update source function for plotting

--- src/usgdp_plot_functions.py
+++ src/usgdp_plot_functions.py
@@ -12,118 +12,33 @@ mpl.rcParams.update(rc_fonts)
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-def nyfed_published_values(input_dir, method='backcast', start_q='2016Q1',
-                           end_q='2021Q4'):
+    
+def find_rmse(true_values, predictions):
     '''
-    Prepare data from the published NYFed Staff Forecast, downloadable from
-    https://www.newyorkfed.org/research/policy/nowcast
-    
-    Parameters  input_dir: str/Path of the directory of the data
-                method: either backcast previous quarter or nowcast current 
-                        quarter
-                start_q: String in the form YYYYQX, for X in {1, 2, 3, 4} 
-                         indicating the start of the period of interest
-                end_q: String in the form YYYYQX, for X in {1, 2, 3, 4} 
-                       indicating the end of the period of interest
-    Returns     df_nyfed: dataframe with nowcast/backcast from NY Fed  
-    '''
-    
-    df_nyfed = pd.read_excel(f'{str(input_dir)}/'\
-                             'New-York-Fed-Staff-Nowcast_data_2002-'
-                             'present.xlsx',
-                             sheet_name='Forecasts By Horizon', skiprows=13,
-                             usecols=['Forecast date', 'Reference quarter',
-                                      'Backcast\n(previous quarter)',
-                                      'Nowcast\n(current quarter)'])
-
-    df_nyfed = df_nyfed.rename({'Backcast\n(previous quarter)':'backcast',
-                                'Nowcast\n(current quarter)':'nowcast',
-                                'Forecast date': 'date',
-                                'Reference quarter': 'quarter'}, axis=1)
-
-    df_nyfed['date'] = pd.to_datetime(df_nyfed['date'])
-    df_nyfed = df_nyfed[['date', 'quarter', method]].dropna()
-    
-    # Reduce to period of interest
-    df_nyfed = df_nyfed[df_nyfed['quarter'].between(start_q, end_q)]
-    df_nyfed.reset_index(drop=True, inplace=True)
-    
-    return df_nyfed
-
-
-def plot_usgdp(df_sig, df_dfm, df_nyfed, ind1, ind2, save_dir=None,
-               filename=None):
-    '''
-    Plot US GDP results between specified indices to compare the signature
-    method against our dynamic factor model and the published values from the 
-    NY Fed.
-    
-    Parameters  df_sig: dataframe with the true GDP values and the signature
-                        results
-                df_dfm: dataframe with the dynamic factor model result
-                df_nyfed: dataframe with the publised NY Fed values
-                ind1: integer indicating the start index
-                ind2: integer indicating the end index
-                save_dir: Path object indicating directory to save to, if left
-                          as default None, then the plot is not saved, and the
-                          filename is redundent
-                filename: string specifying the filename    
-    '''
-        
-    plt.step(df_sig['date'][ind1:ind2], df_sig['true_value'][ind1:ind2], 
-             marker = None, label='GDP', markersize=12, linewidth=3,
-             where='post')
-    plt.step(df_sig['date'][ind1:ind2], df_sig['prediction'][ind1:ind2], 
-             marker = '.', label='Sigs', markersize=12, linewidth=1.5,
-             where='post')
-    plt.step(df_nyfed['date'][ind1:ind2], df_nyfed['nowcast'][ind1:ind2], 
-             marker = 'x', label='NYFed', markersize=12, linewidth=1.5,
-             where='post')
-    plt.step(df_dfm['date'][ind1:ind2], df_dfm['dfm_prediction'][ind1:ind2],
-             marker = '^', label='DFM', markersize=12, linewidth=1.5, 
-             where='post')
-    plt.xticks(rotation=30)
-    plt.ylabel('value')
-    plt.legend()
-    
-    if save_dir:
-        if not filename:
-            filename = 'USGDP_results'
-        
-        plt.savefig(f'{save_dir}/{filename}.pdf', bbox_inches='tight')
-    
-    plt.show()
-    
-    
-def find_rmse(true_values, predictions, ind1, ind2):
-    '''
-    Finds the RMSE between 2 specified indices with the end points included
+    Finds the RMSE between two Series
     
     Parameters  true_values: pandas Series of true values 
                 predictions: pandas Series of predicted values 
-                ind1: integer indicating the start index
-                ind2: integer indicating the end index
     Returns     rmse: float, the root mean square error 
     
     '''
-    rss = np.sum((true_values[ind1:ind2+1]-predictions[ind1:ind2+1])**2)
-    rmse = np.sqrt(rss/(ind2-ind1+1))
+    rss = np.sum((true_values-predictions)**2)
+    rmse = np.sqrt(rss/(len(true_values)))
     
     return rmse
 
 
-def plot_config(config_folder, experiment, ind1=0, ind2=100, save_dir=None, 
+def plot_config(config_folder, experiment_list, start_date, end_date, save_dir=None, 
                 filename=None, input_dir=None, results_dir=None):
     
     '''
-    plots the results for a particular setting
+    plots the results for a particular experiment_list
     
     Parameters  config_folder: string giving the folder name of the specific 
                                config was saved in
-                experiment: string giving the experiment
-                ind1: integer indicating the start index
-                ind2: integer indicating the end index
+                experiment_list: list of tuples specifying each experiment_name and corresponding label
+                start_date: pandas datetime object indicating the start date
+                end_date: pandas datetime object indicating the end date
                 save_dir: Path object indicating directory to save to, if left
                           as default None, then the plot is not saved, and the
                           filename is redundent
@@ -141,66 +56,91 @@ def plot_config(config_folder, experiment, ind1=0, ind2=100, save_dir=None,
     if not results_dir:
         results_dir = Path(__file__).resolve().parent.parent/'results'
     
-    df_nyfed = nyfed_published_values(input_dir, method='nowcast')
+    # Load results for the DFM baseline
     dfm_file = results_dir/'dfm'/'dfm_predictions.yaml'
 
     with open(dfm_file, 'r') as stream:
             dfm_results = yaml.safe_load(stream)
     df_dfm = pd.DataFrame(dfm_results.items(), columns=['date', 
                                                         'dfm_prediction'])
-
-    df_sig = pd.read_csv(results_dir/experiment/config_folder\
-                         /'all_predictions.csv')
-    df_sig['date'] = pd.to_datetime(df_sig['date'], format="%Y_%m_%d")
     df_dfm['date'] = pd.to_datetime(df_dfm['date'], format="%Y-%m-%d")
+    df_dfm = df_dfm[(df_dfm['date'] >= start_date) & (df_dfm['date'] <= end_date)]
     
-    plot_usgdp(df_sig, df_dfm, df_nyfed, ind1, ind2, save_dir=save_dir,
-               filename=filename)
-    
-    all_rmse = {}
-    
-    rmse_sig = find_rmse(df_sig['true_value'], df_sig['prediction'], ind1,
-                         ind2)
-    all_rmse['Sig'] = rmse_sig
-    
-    rmse_dfm = find_rmse(df_sig['true_value'], df_dfm['dfm_prediction'], ind1,
-                         ind2)
-    all_rmse['DFM'] = rmse_dfm
+    all_dfs = []
+    true_column = None
 
-    rmse_ny = find_rmse(df_sig['true_value'], df_nyfed['nowcast'], ind1, ind2)
-    all_rmse['NYFed'] = rmse_ny
-    
-    df_rmse = pd.DataFrame(all_rmse.items(), columns=['method', 'rmse'])
-    
-    return df_rmse
+    for experiment, label in experiment_list:
+        # Load results for each new model
+        filtered_df = load_df(results_dir, experiment, config_folder, start_date, end_date)
 
+        # Plot the true GDP values if it hasn't been plotted already
+        if (true_column is None) and ('true_value' in filtered_df.columns):
+            true_column = filtered_df['true_value']
+            plot_df(filtered_df, label='True Value', pred_column = 'true_value', point_size = 8, line_width = 3, marker=None)
 
-def plot_average_quarter(results_dir, config_folder, experiment, 
-                         save_dir=None, filename=None):
-    '''
-    Plots the average error as the quarter progresses
+        # Plot each new model results and append results to list
+        plot_df(filtered_df, label)
+        all_dfs.append((filtered_df, label))
+
+    # Plot DFM baseline results and append results to list
+    all_dfs.append((df_dfm, 'Model 5 Baseline'))
+    plot_df(df_dfm, 'Model 5 Baseline', 'dfm_prediction')  
     
-    Parameters  results_dir: Path where the results are saved
-                config_folder: string of config folder name
-                experiment: string giving the name of the experiment
-                save_dir: Path object indicating directory to save to, if left
-                          as default None, then the plot is not saved, and the
-                          filename is redundent
-                filename: string specifying the filename 
-    '''
-    
-    df_sig = pd.read_csv(results_dir/experiment/config_folder/\
-                         'all_predictions.csv')
-    
-    df_sig['date'] = pd.to_datetime(df_sig['date'], format="%Y_%m_%d")
-    df_sig['quarter'] = df_sig['date'].dt.to_period('Q')
-    df_sig['q_week'] = df_sig.groupby('quarter').cumcount()+1
-    df_sig['error'] = np.abs(df_sig['true_value'] - df_sig['prediction'])
-    
-    plt.plot(df_sig[['error', 'q_week']].groupby('q_week').mean())
-    
+    plt.xticks(rotation=30)
+    plt.ylabel('GDP Growth (%)', fontsize='small')
+    plt.legend(loc=2, fontsize=10)
+
+    # Save plot
     if save_dir:
         if not filename:
             filename = 'USGDP_results'
         
         plt.savefig(f'{save_dir}/{filename}.pdf', bbox_inches='tight')
+    
+    plt.show()
+    
+    # Calculate RMSE for each model
+    all_rmse = {}
+    for df, label in all_dfs:
+        pred_column = 'prediction' if 'prediction' in df.columns else 'dfm_prediction'
+        rmse = find_rmse(true_column, df[pred_column])
+        all_rmse[label] = rmse
+
+    df_rmse = pd.DataFrame(all_rmse.items(), columns=['method', 'rmse'])
+    
+    return df_rmse
+
+def load_df(results_dir, experiment, config_folder, start_date, end_date):
+    '''
+    Load and filter US GDP results based on the date range
+    
+    Parameters:
+    - results_dir: Path of where the results are stored
+    - experiment: string indicating experiment name of current data
+    - config_folder: string giving the folder name of the specific 
+                               config was saved in
+    - start_date: pandas datetime object indicating the start date
+    - end_date: pandas datetime object indicating the end date
+    Returns     filtered_df: dataframe with the filtered date range
+    '''
+
+    current_df = pd.read_csv(results_dir/experiment/'pca'/'rerun'/config_folder\
+                        /'all_predictions.csv')
+    current_df['date'] = pd.to_datetime(current_df['date'], format="%Y_%m_%d")
+    filtered_df = current_df[(current_df['date'] >= start_date) & (current_df['date'] <= end_date)]
+    return filtered_df
+
+def plot_df(filtered_df, label, pred_column = 'prediction', point_size = 8, line_width = 2, marker='.'):
+    '''
+    Plot US GDP results based on a pandas dataframe
+    
+    Parameters:
+    - filtered_df: dataframe containing data to be plotted
+    - label: string to be used as label in plot legend
+    - pred_column: string indicating the column name of results in the dataframe
+    - point_size: string indicating the size of points on the line in the plot
+    - line_width: string indicating the width of the line
+    - marker: string indicating the type of marker to use on the line
+    '''
+    plt.step(filtered_df['date'], filtered_df[pred_column],
+        marker=marker, label=label, markersize=point_size, linewidth=line_width, where='post')
